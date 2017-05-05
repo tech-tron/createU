ce7ff5a566f49a337909838191495327c1aefcca
tech-tron <phillables@gmail.com>
2017-05-05 17:55:58 -0400

    add form element var -js

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
 //input variables
+var genderMale = document.getElementById('male');
+var genderFemale  = document.getElementById(
+'female');
+
+var fName  = document.getElementById('firstName');
+var lName  = document.getElementById('lastName');
+
+var bDay = document.getElementById('birthdate');
+
+var hBlonde = document.getElementById('blonde');
+var hBurnette = document.getElementById('burnette');
+var hGinger = document.getElementById('ginger');
+var hblack = document.getElementById('black');
+var hGrey = document.getElementById('grey');
+
+var iBlue = document.getElementById('blue');
+var iGreen = document.getElementById('green');
+var iHazel = document.getElementById('hazel');
+var iBrown = document.getElementById('brown');
+
+var subBut = document.getElementById('submit_people');
+
+//hair button
+
+
 
 /*
   active class drops down the button
