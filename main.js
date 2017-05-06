c32a99c7f2fede4635a1241450e83c6022a4b75e
tech-tron <phillables@gmail.com>
2017-05-05 22:07:41 -0400

    add male/female.onclick()

--- main.js
+++ main.js
@@ -21,9 +21,12 @@ var iBrown = document.getElementById('brown');
 
 var subBut = document.getElementById('submit_people');
 
-//hair button
+var gender = '';
+var firstName = '';
+var lastName = '';
 
-
+genderMale.onclick = getGender;
+genderFemale.onclick = getGender;
 
 /*
   active class drops down the button
@@ -32,3 +35,28 @@ var subBut = document.getElementById('submit_people');
   others on hover upsize slightly
   //buttonEngaged(){};
 */
+if(gender != ""){
+
+}
+
+//methods
+function getGender() {
+  gender = this.id
+  this.style.height = '50px';
+  document.getElementById('gender').style.margin = '0';
+  if(this.id == genderMale.id){
+    this.style.margin = '0';
+    this.style.width = '59%';
+    this.style.backgroundColor = "#99ffff";
+    this.style.left = '0';
+  }else{
+    this.style.margin = '0';
+    this.style.width = '59%';
+    this.style.backgroundColor = "#ffb6c1";
+    this.style.left = '0';
+  }
+
+}
+
+
+ console.log('JS is included');
