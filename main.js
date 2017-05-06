c10cdba326639745c35df8e5cf894fc10556137f
tech-tron <phillables@gmail.com>
2017-05-05 23:08:52 -0400

    add input submit -not age

--- main.js
+++ main.js
@@ -1,32 +1,33 @@
 //input variables
+var people = [];
 var genderMale = document.getElementById('male');
 var genderFemale  = document.getElementById(
 'female');
 
-var fName  = document.getElementById('firstName');
-var lName  = document.getElementById('lastName');
-
 var bDay = document.getElementById('birthdate');
 
-var hBlonde = document.getElementById('blonde');
-var hBurnette = document.getElementById('burnette');
-var hGinger = document.getElementById('ginger');
-var hblack = document.getElementById('black');
-var hGrey = document.getElementById('grey');
+document.getElementById('blonde').onclick = setHairColor;
+document.getElementById('burnette').onclick = setHairColor;
+document.getElementById('ginger').onclick = setHairColor;
+document.getElementById('black').onclick = setHairColor;
+document.getElementById('grey').onclick = setHairColor;
 
-var iBlue = document.getElementById('blue');
-var iGreen = document.getElementById('green');
-var iHazel = document.getElementById('hazel');
-var iBrown = document.getElementById('brown');
+document.getElementById('blue').onclick = setEyeColor;
+document.getElementById('green').onclick = setEyeColor;
+document.getElementById('hazel').onclick = setEyeColor;
+document.getElementById('brown').onclick = setEyeColor;
 
 var subBut = document.getElementById('submit_people');
 
 var gender = '';
-var firstName = '';
-var lastName = '';
+var iColor = '';
+var hColor = '';
+var age = '';
+
 
 genderMale.onclick = getGender;
 genderFemale.onclick = getGender;
+//bDay.onclick = getAge;
 
 /*
   active class drops down the button
@@ -35,15 +36,35 @@ genderFemale.onclick = getGender;
   others on hover upsize slightly
   //buttonEngaged(){};
 */
-if(gender != ""){
+
+subBut.onclick = function(){
+  char5 = gender;
+  char4 = iColor;
+  char3 = hColor;
+  char2 = age;
+  char1 = document.getElementById('firstName').value;
+  char0 = document.getElementById('lastName').value;
+
+  people.push(new Person(char0, char1, char2, char3, char4, char5));
+}
+count = 0;
+//methods
+function Person(fName, lName, age, hColor, iColor, sex){
+  this.firstName =fName;
+  this.lastName = lName;
+  this.age = age;
+  this.hair = hColor;
+  this.eyes = iColor;
+  this.gender = sex;
 
 }
 
-//methods
+
 function getGender() {
   gender = this.id
   this.style.height = '50px';
   document.getElementById('gender').style.margin = '0';
+  console.log(gender);
   if(this.id == genderMale.id){
     this.style.margin = '0';
     this.style.width = '59%';
@@ -58,5 +79,18 @@ function getGender() {
 
 }
 
+function getAge() {
+  age = 'young';
+}
+
+function setEyeColor () {
+  iColor = this.id;
+  console.log(iColor);
+}
+
+function setHairColor () {
+  hColor = this.id;
+  console.log(hColor);
+}
 
  console.log('JS is included');
